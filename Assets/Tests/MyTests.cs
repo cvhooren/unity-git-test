388bcc5ea5df36d7e4a8799594f17ef5362fdf28
Colin van Hooren <c.vanhooren@student.fontys.nl>
2021-02-15 09:43:43 +0100

    Add test test

---
+++ Assets/Tests/MyTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class MyTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void MySimplePasses()
+        {
+            Assert.AreEqual(5, 5);
+        }
+    }
+}
